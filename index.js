3a379fa3eca934cfb0fe606dbea4769bb33c4bbc
bree-nault <60358464+bree-nault@users.noreply.github.com>
2020-06-09 01:04:22 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function showAbout() {
+    $('.portfolioSec').hide();
+    $('.aboutSec').show();
+   
+  }
+  
+  $("#portfolio").click(function(){
+               $(".aboutSec").hide();
+               $(".portfolioSec").show();
+  
+          });
+  
+        $("#about").click(function(){
+               $(".portfolioSec").hide();
+               $(".aboutSec").show();
+  
+  
+          });  
+  $(showAbout);
